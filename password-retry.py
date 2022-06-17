493852500894179cee843106dee2a0a9ca434f1f
Pao Tsung <wannaflyx3@gmail.com>
2022-06-18 02:08:32 +0800

    change to while >0

--- password-retry.py
+++ password-retry.py
@@ -5,7 +5,7 @@
 #如果正確 就印出"登入成功"
 #如果不正確 進印出"密碼錯誤，還有__次機會
 
-password = 'a123456'
+'''password = 'a123456'
 i = 3 #剩餘機會
 while True:
 	password1 = input('請輸入密碼:')
@@ -17,8 +17,20 @@ while True:
 		i -= 1
 		print('密碼錯誤，還有',i,'次機會')
 		if i == 0:
-			break
-		
+			break'''
+
+password = 'a123456'
+i = 3 #剩餘機會
+while i > 0:
+	password1 = input('請輸入密碼:')
+	
+	if password == password1:
+		print('登入成功')
+		break  #跳出迴圈
+	else:
+		i -= 1
+		print('密碼錯誤，還有',i,'次機會')
+
 
 
 
