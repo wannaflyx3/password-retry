7f1cdcbd9dd87f4ba252af5d4970233e351d0bf2
Pao Tsung <wannaflyx3@gmail.com>
2022-06-18 02:14:08 +0800

    不再顯示剩餘0次

--- password-retry.py
+++ password-retry.py
@@ -22,14 +22,18 @@ while True:
 password = 'a123456'
 i = 3 #剩餘機會
 while i > 0:
+	i -= 1
 	password1 = input('請輸入密碼:')
 	
 	if password == password1:
 		print('登入成功')
 		break  #跳出迴圈
 	else:
-		i -= 1
-		print('密碼錯誤，還有',i,'次機會')
+		print('密碼錯誤')
+		if i > 0:
+			print('還有',i,'次機會')
+		else:
+			print('你的帳戶已鎖定!')
 
 
 
